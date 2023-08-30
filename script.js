12387e82def1e2e1a1ba9ac01bcfa77fd1bc445d
IgorBern02 <igor.bernardes2@gmail.com>
2023-08-29 23:18:46 -0300

    First Commit

---
+++ script.js
@@ -0,0 +1,34 @@
+// Relógio Digital
+const hour = document.getElementById("hour");
+const min = document.getElementById("min");
+const sec = document.getElementById("sec");
+
+setInterval(() => {
+  let date = new Date();
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  let seconds = date.getSeconds();
+
+  hour.innerHTML = `${formatTime(hours)}`;
+  min.innerHTML = `${formatTime(minutes)}`;
+  sec.innerHTML = `${formatTime(seconds)}`;
+
+//   document.getElementById("hour").innerHTML = `${formatTime(hours)} : ${formatTime(minutes)} : ${formatTime(seconds)}`;  
+}, 1000);
+
+function formatTime(time) {
+  return time < 10 ? "0" + time : time;
+}
+
+//  Dias/Mês/Ano
+dayName = new Array ("domingo", "Segunda-feira", "Terça-feira", "Quarta-feira", "Quinta-feira", "Sexta-feira", "Sábado")
+monName = new Array ("janeiro", "fevereiro", "março", "abril", "maio", "junho", "agosto", "outubro", "novembro", "dezembro")
+
+var data = new Date();
+var dayText = String(dayName[data.getDay()]).padStart(2, "0");
+var day = String(data.getDate()).padStart(2, "0");
+var month = String(monName[data.getMonth()]).padStart(2, "0");
+var year = data.getFullYear();
+
+document.getElementById("dayText").innerHTML = `${dayText}`
+document.getElementById("date").innerHTML = `${day} de ${month} de ${year}`;
